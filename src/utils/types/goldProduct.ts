a16c1e572b9dda9c6112931c5c70a009bc3ce6df
Gökhan Alkacir <gokhan@pija.se>
2022-12-09 13:40:49 +0100

    feat: add gold as a special type of product

---
+++ src/utils/types/goldProduct.ts
@@ -0,0 +1,12 @@
+// title: ""
+// productId: "se.pija.Quiz.dev.100"
+// goldAmount: 100
+// adFreeDays: 30
+export type GoldProduct = {
+  title: string;
+  productID: string;
+  price: number;
+  adFreeSeconds: number;
+  goldAmount: number;
+  bonusGoldAmount?: number;
+}
