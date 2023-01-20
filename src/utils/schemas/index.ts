8d2f9e713583d4bd2d6fa8d4873b1ced2aa8dc0c
Gökhan Alkacir <gokhan@pija.se>
2023-01-20 17:23:14 +0100

    feat: bumped verson, exported question schema

--- src/utils/schemas/index.ts
+++ src/utils/schemas/index.ts
@@ -1,2 +1,3 @@
 export { default as GoldProductSchema } from './goldProductSchema';
 export { default as UserSchema } from './userSchema';
+export { default as QuestionSchema } from './questionSchema';
