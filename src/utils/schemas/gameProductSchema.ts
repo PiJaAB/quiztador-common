f5619cc165b9fc0f8bf2ee22fb816b49e9221735
Gökhan Alkacir <gokhan@pija.se>
2023-01-31 15:10:04 +0100

    feat: changed structure of game product

--- src/utils/schemas/gameProductSchema.ts
+++ src/utils/schemas/gameProductSchema.ts
@@ -1,18 +1,14 @@
 import { z } from 'zod';
 
-// E.g "Headgear"
 const GameProductSchema = z.object({
   type: z
     .union([z.literal('CONSUMABLE'), z.literal('NON-CONSUMABLE')])
     .default('NON-CONSUMABLE'),
   category: z.string(),
+  title: z.string(),
+  price: z.number(),
   imageUrl: z.string().optional(),
-  item: z.object({
-    color: z.string().optional(),
-    title: z.string(),
-    price: z.number(),
-  }),
-  unlockLevel: z.number(),
+  bundledImage: z.string().optional(),
 });
 
 export default GameProductSchema;
