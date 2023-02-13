500ed1bc51513895b6c449e7241c89c6b5379a45
Gökhan Alkacir <gokhan@pija.se>
2023-02-13 10:25:41 +0100

    feat: SKU (stock keeping unit) on game product

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -4,3 +4,4 @@ export * from './schemas';
 export { default as segment } from './segment';
 export { default as isObject } from './isObject';
 export { default as seedRandom } from './seedRandom';
+export { default as gameProductBelongToCategory } from './gameProductBelongToCategory';
