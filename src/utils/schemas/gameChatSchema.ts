36964b9ac32d781acdd22bb53948977a83f35010
Gökhan Alkacir <gokhan@pija.se>
2023-04-12 17:03:19 +0200

    feat: WIP chat for in-game

---
+++ src/utils/schemas/gameChatSchema.ts
@@ -0,0 +1,41 @@
+import { z } from 'zod';
+
+const ReplySchema = z.object({
+  title: z.string(),
+  value: z.string(),
+  messageId: z.any().optional(),
+});
+
+const QuickRepliesSchema = z.object({
+  type: z.union([z.literal('radio'), z.literal('checkbox')]),
+  values: z.array(ReplySchema),
+  keepIt: z.boolean().optional(),
+});
+
+const GameChatSenderSchema = z.object({
+  _id: z.union([z.string(), z.number()]),
+  name: z.string().optional(),
+  avatar: z.union([z.string(), z.number()]).optional(),
+});
+
+export const GameChatMessageSchema = z.object({
+  _id: z.union([z.string(), z.number()]),
+  text: z.string(),
+  createdAt: z.union([z.date(), z.number()]),
+  user: GameChatSenderSchema,
+  image: z.string().optional(),
+  video: z.string().optional(),
+  audio: z.string().optional(),
+  system: z.boolean().optional(),
+  sent: z.boolean().optional(),
+  received: z.boolean().optional(),
+  pending: z.boolean().optional(),
+  quickReplies: QuickRepliesSchema.optional(),
+});
+
+export const GameChatSchema = z.object({
+  createdAt: z.string(),
+  gameRef: z.string(),
+  text: GameChatMessageSchema,
+  sender: GameChatSenderSchema,
+});
--- src/utils/schemas/index.ts
+++ src/utils/schemas/index.ts
@@ -3,6 +3,7 @@ export * from './participantSchema';
 export * from './gameSchema';
 export * from './categorySchema';
 export * from './gameProductSchema';
+export * from './gameChatSchema';
 
 export { default as AppStoreProductSchema } from './appStoreProductSchema';
 export { default as QuestionSchema } from './questionSchema';
