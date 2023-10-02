432f0275f743d1539ed44904f44f484dffdef061
Alexander Lind <smulan300@gmail.com>
2023-10-02 16:43:29 +0200

    created and updated at quesiton

--- src/utils/schemas/questionSchema.ts
+++ src/utils/schemas/questionSchema.ts
@@ -1,6 +1,13 @@
 import { z } from 'zod';
 import QuestionStatuses from './questionStatuses';
 
+const stringOrDate = z.custom((data) => {
+  if (typeof data === 'string' || data instanceof Date) {
+    return data;
+  }
+  throw new Error('Invalid type. Must be a string or a Date object.');
+});
+
 const QuestionSchema = z.object({
   category: z.string(),
   question: z.string(),
@@ -11,6 +18,8 @@ const QuestionSchema = z.object({
   wrongAnswer3: z.string(),
   status: QuestionStatuses,
   author: z.string(),
+  createdAt: stringOrDate.optional(),
+  updatedAt: stringOrDate.optional(),
   votes: z.number().optional(),
   nid: z.string().optional(),
   lastTenAnswers: z
